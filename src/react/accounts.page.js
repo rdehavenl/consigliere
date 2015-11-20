a557cb831cb107561304877a636db7c9955f7b3b
Ravi Nair <ivarrian@gmail.com>
2015-11-21 01:30:34 +1100

    Removing bogus data

--- src/react/accounts.page.js
+++ src/react/accounts.page.js
@@ -3,12 +3,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var AccountsPage = require('../components/AccountsPage.react');
 
-var accountsList = [
-  {name:'Account-A',number:'123456'},
-  {name:'Account-B',number:'654321'}
-]
 // Render the components, picking up where react left off on the server
 ReactDOM.render(
-  <AccountsPage accounts={accountsList} />,
+  <AccountsPage />,
   document.getElementById('accounts-react')
 );
