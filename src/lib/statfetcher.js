b4b741b5d913ad076694b9703c42c7e8c94bca42
Ravi Nair <ivarrian@gmail.com>
2015-11-30 23:38:53 +1100

    Fetch checks and summaries and store in redis

--- src/lib/statfetcher.js
+++ src/lib/statfetcher.js
@@ -1,7 +1,44 @@
 var StatFetcher = {};
+var aws = require('aws-sdk');
+var auth = require('./auth');
+var config = require('config');
+var redis = require("redis"),
 
-StatFetcher.fetchStatsFor = function(accountNumber){
-  console.log("doSomething says "+accountNumber);
+client = redis.createClient(process.env.REDIS_URL || 'redis://127.0.0.1/6379');
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+StatFetcher.fetchStatsFor = function(account){
+  auth.getSupport(account,function(err,support){
+    var params = {
+      language: config.Defaults.AWS.Support.Language
+    };
+    support.describeTrustedAdvisorChecks(params, function(err, data) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        client.set(account.accountNumber+'_checks', JSON.stringify(data));
+        var checkIds = [];
+        data.checks.forEach(function(check){
+          checkIds.push(check.id);
+        });
+        var params = {
+          checkIds : checkIds
+        };
+        support.describeTrustedAdvisorCheckSummaries(params, function(err, data) {
+          if (err){
+            console.log(err);
+          }
+          else {
+            client.set(account.accountNumber+'_summaries', JSON.stringify(data));
+          }
+        });
+      }
+    });
+  });
 }
 
 module.exports = StatFetcher;
