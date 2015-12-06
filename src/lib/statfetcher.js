1979a92596a3bedaeee7833393b9e4820f4f7581
Ravi Nair <ivarrian@gmail.com>
2015-12-07 09:24:00 +1100

    Fetch consolidated stats for category

--- src/lib/statfetcher.js
+++ src/lib/statfetcher.js
@@ -4,6 +4,7 @@ var auth = require('./auth');
 var config = require('config');
 var redis = require("redis");
 var async = require('async');
+var _ = require('lodash');
 var mAccounts = require('../models/accounts');
 
 
@@ -61,6 +62,117 @@ StatFetcher.getStatsForAccount = function(account,callback){
   client.keys(account.accountNumber+'_*',function(err,replies){
     if(!err){
       console.log(replies);
+    }
+    else {
+      callback(err);
+    }
+  });
+}
+
+StatFetcher.getSummaryForCategoryForAll = function(category,callback){
+  mAccounts.find({},function(err,accounts){
+    if(!err){
+      var calls = [];
+      var checks = [];
+      var summaries = [];
+      accounts.forEach(function(account){
+        calls.push(function(hollaback){
+          client.get(account.accountNumber+'_checks',function(err,checkList){
+            if(!err){
+              var returnedChecks = JSON.parse(checkList);
+              returnedChecks.checks.forEach(function(check){
+                if(check.category == category){
+                  checks.push({accountName:account.accountName,accountNumber:account.accountNumber,check:check});
+                }
+              });
+              client.get(account.accountNumber+'_summaries',function(err,summaryList){
+                if(!err){
+                  var returnedSummaries = JSON.parse(summaryList);
+                  returnedSummaries.summaries.forEach(function(summary){
+                    summaries.push({accountName:account.accountName, accountNumber:account.accountNumber, summary:summary});
+                  });
+                  hollaback();
+                }
+                else {
+
+                  hollaback(err);
+                }
+              });
+            }
+            else {
+              hollaback(err);
+            }
+          });
+        });
+      });
+      async.parallel(calls,function(err,res){
+        if(!err){
+          calls = [];
+          var detailedChecks = [];
+          checks.forEach(function(check){
+            calls.push(function(hollaback){
+              client.get(check.accountNumber+'_result_'+check.check.id, function(err,res){
+                if(!err){
+                  var returnedCheckDetail = JSON.parse(res);
+                  detailedChecks.push({accountNumber:check.accountNumber, accountName:check.accountName,result:returnedCheckDetail.result});
+                  hollaback();
+                }
+                else {
+                  hollaback(err);
+                }
+              });
+            });
+          });
+          async.parallel(calls,function(err,res){
+            if(!err){
+              // console.log('%j',detailedChecks);
+              // consolidate checks by name
+              var consolidatedChecks = [];
+              checks.forEach(function(check){
+                found = false;
+                foundIndex = -1;
+                consolidatedChecks.forEach(function(consolidatedCheck,index){
+                  if(consolidatedCheck.name == check.check.name){
+                    found = true;
+                    foundIndex = index;
+                  }
+                });
+                var foundSummary;
+                summaries.forEach(function(summary){
+                  if(summary.summary.checkId == check.check.id && summary.accountNumber == check.accountNumber){
+                    foundSummary = summary.summary;
+                  }
+                });
+                var foundDetail;
+                detailedChecks.forEach(function(detailedCheck){
+                  if(detailedCheck.accountNumber == check.accountNumber && detailedCheck.result.checkId){
+                    foundDetail = detailedCheck.result;
+                  }
+                })
+                var mergedCheckSummary = _.merge(foundSummary,check.check);
+                mergedCheckSummary = _.merge(mergedCheckSummary,foundDetail);
+                mergedCheckSummary = _.merge(mergedCheckSummary,{accountNumber:check.accountNumber,accountName:check.accountName});
+                if(found == true){
+                  // name exists, add check to it.
+                  consolidatedChecks[foundIndex].checks.push(mergedCheckSummary);
+                }
+                else {
+                  // name doesn't exist yet, create it with a single element check array
+                  consolidatedChecks.push({name:check.check.name, checks:[mergedCheckSummary]});
+                }
+              });
+              callback(null,consolidatedChecks);
+            }
+            else {
+              callback(err);
+            }
+          });
+        }
+        else {
+          callback(err);
+        }
+      });
+
     }
     else {
       callback(err);
