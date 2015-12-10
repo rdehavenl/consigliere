cb66c8120733565a9ed329dbfe5ea228dddcc566
Ravi Nair <ivarrian@gmail.com>
2015-12-11 09:24:06 +1100

    Flagged resource summary per account

--- src/components/Check/AccountGroup.react.js
+++ src/components/Check/AccountGroup.react.js
@@ -1,9 +1,40 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var config = require('../config/config');
 
 module.exports = React.createClass({
+  getDefaultText: function(){
+    console.log(this.props.check.name);
+    var defText = null;
+    var supText = null;
+    config.TrustedAdvisor.Checks.forEach(function(check){
+      if(check.Name == this.props.check.name){
+        defText = check.DefaultText;
+        supText = check.SuppressionText;
+      }
+    }.bind(this));
 
+    if(defText != null){
+      if(typeof this.props.check.resourcesSummary != 'undefined'){
+        defText = defText.replace("%X",this.props.check.resourcesSummary.resourcesFlagged);
+        defText = defText.replace("%Y",this.props.check.resourcesSummary.resourcesProcessed);
+        supText = supText.replace("%X",this.props.check.resourcesSummary.resourcesSuppressed);
+        if(this.props.check.resourcesSummary.resourcesSuppressed > 0)
+          return defText+' '+supText;
+        else
+          return defText;
+      }
+      else {
+        return "N/A";
+      }
+
+    }
+    else {
+      return "N/A";
+    }
+  },
   render: function(){
+    var defaultText = this.getDefaultText();
     var statusSpan;
     switch(this.props.check.status){
       case 'ok':
@@ -19,6 +50,7 @@ module.exports = React.createClass({
         statusSpan = <span className="pull-right text-right label label-default">Not Available</span>
       break;
     }
+
     var accordionId = 'accountgroup_accordion' + this.props.hash;
     var collapseId = 'accountgroup_collapse' + this.props.hash;
     var hashAccordionId = '#' + accordionId;
@@ -36,7 +68,7 @@ module.exports = React.createClass({
           </div>
           <div id={collapseId} className="panel-collapse collapse">
             <div className="panel-body">
-              Blah Blah
+                {defaultText}
             </div>
           </div>
         </div>
