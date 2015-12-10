6cbd04ea19918879c58d55996f3d62d8eef94e47
Ravi Nair <ivarrian@gmail.com>
2015-12-11 08:58:20 +1100

    Adding watch task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,47 @@
 var gulp = require('gulp');
-var mkdirp = require('less-mkdirp');
-var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var esnext = require('gulp-esnext');
-var mocha = require('gulp-mocha');
-var bower = require('gulp-bower');
+var react = require('gulp-react');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
+var browserify = require('browserify');
+var watchify = require('watchify');
 
-gulp.task('default', function(){
-  mkdirp('data',function(err){
-    if(!err){
-      return gulp.src('src/*/*.js') // read all of the files that are in script/lib with a .js extension
-        .pipe(esnext())
-        .pipe(jshint()) // run their contents through jshint
-        // .pipe(jshint.reporter('default')) // report any findings from jshint
-        .pipe(concat('all.js')) // concatenate all of the file contents into a file titled 'all.js'
-        .pipe(gulp.dest('dist/js')) // write that file to the dist/js directory
-        .pipe(rename('all.min.js')) // now rename the file in memory to 'all.min.js'
-        .pipe(uglify()) // run uglify (for minification) on 'all.min.js'
-        .pipe(gulp.dest('dist/js')); // write all.min.js to the dist/js file
-    }
-  });
+var path = {
+  ALL: ['src/**/*.js', 'src/views/**/*.html'],
+  JS: ['src/components/**/*.js', 'src/react/*.js'],
+  MINIFIED_OUT: 'bundled.js',
+  DEST_SRC: 'dist/src',
+  DEST_BUILD: 'src/public/scripts/',
+  DEST: 'dist',
+  ENTRY_POINT: 'src/react/app.js',
+  OUT: 'build.js'
+};
+
+gulp.task('transform', function(){
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 
 gulp.task('test', function(){
-  // bower({ cmd: 'update'});
   return gulp.src('test/**/*.js', {read: false})
     .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(path.JS, ['build']);
+});
+
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+    .pipe(gulp.dest(path.DEST_BUILD));
 });
