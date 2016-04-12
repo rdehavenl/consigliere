9fdd7aa1f54385f418d1da1aacd3668186e956be
Daniel Koo <koo.daniel@gmail.com>
2016-04-12 14:10:26 +1000

    added the config dir to npm test fixed the tests, most of which were for Mongoose, instead of for Dynamoose

--- test/models/accounts.js
+++ test/models/accounts.js
@@ -6,7 +6,7 @@ var accounts = require('../../src/models/accounts');
 describe("Accounts Model", function() {
   describe("Purge database", function(){
     it("Purges the database", function(done){
-      accounts.remove({},function(err){
+      accounts.delete({accountNumber: 100000000000000000000, accountName: 'ASDFGHJKL'},function(err){
         should.not.exist(err);
         done();
       });
@@ -28,14 +28,14 @@ describe("Accounts Model", function() {
   });
   describe("Add a Duplicate Master Account", function() {
     it("Fails to add duplicate account",function(done){
-      var account = new accounts({
+      var account_params = {
         accountName: 'Shared',
         accountNumber: '12345',
         type: 'master',
         choice:'role'
-      });
-      account.save(function(saveErr){
-        should.exist(saveErr);
+      };
+      accounts.create(account_params, function(err){
+        should.exist(err);
         done();
       });
     });
@@ -57,14 +57,14 @@ describe("Accounts Model", function() {
   });
   describe("Add a duplicate slave Account", function() {
     it("Fails to add a duplicate slave account",function(done){
-      var account = new accounts({
+      var account_params = {
         accountName: 'Slave1',
         accountNumber: '7891',
         type: 'slave',
         choice: 'role',
         roleArn:'arn:something:or:the:other'
-      });
-      account.save(function(err){
+      };
+      accounts.create(account_params, function(err){
         should.exist(err);
         done();
       });
@@ -72,7 +72,7 @@ describe("Accounts Model", function() {
   });
   describe("Gets all accounts", function() {
     it("Gets list of accounts",function(done){
-      accounts.find({},function(err,listOfAccounts){
+      accounts.scan({},function(err,listOfAccounts){
         should.not.exist(err);
         should.exist(listOfAccounts);
         done();
@@ -81,15 +81,11 @@ describe("Accounts Model", function() {
   });
   describe("Gets an account by number", function() {
     it("Gets a single account by number",function(done){
-      accounts.find({accountNumber:'7891'},function(err,account){
+      accounts.query({accountNumber:'7891'},function(err,account){
         should.not.exist(err);
         should.exist(account);
         expect(account[0].accountNumber).to.equal('7891');
-        account[0].close(function(err){
-          if(!err){
-            done();
-          }
-        })
+        done();
       });
     });
   });
